0c782e7e9967de948d79382109c0a99a586f9987
izazahmed0177 <izazahmed0177@gmail.com>
2022-11-24 11:34:18 +0600

    router & toster  add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes/Routes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+       <RouterProvider router={router}></RouterProvider>
+       <Toaster></Toaster>
     
     </div>
   );
